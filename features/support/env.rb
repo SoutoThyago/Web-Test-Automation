a3fd1986173f1909bae28c98d31a84eca222b9ce
SoutoThyago <thyagocsouto@gmail.com>
2019-06-05 01:45:34 -0300

    Remove documentation

--- features/support/env.rb
+++ features/support/env.rb
@@ -3,22 +3,11 @@ require 'selenium-webdriver'
 require 'watir'
 require 'rspec'
 
-# Default chrome
-
-#browser = Watir::Browser.new
-#browser.driver.manage.window.maximize
-
-#Before do 
-    # variavel global chamada @browser link no browser
-#    @browser = browser
-#end
-
-
 Selenium::WebDriver::Chrome.driver_path="C:/chromedriver.exe"
 driver = Selenium::WebDriver.for :chrome
 driver.manage.window.maximize
 
 Before do 
-    # variavel global @browser linkada no browser
+    # variavel global 
     @driver = driver
 end
